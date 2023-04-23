41970b0053330d746ce073e257e8128d4dcf0744
dflk <904885508@qq.com>
2023-04-23 20:55:47 +0800

    first commit

---
+++ src/main/java/com/westee/cake/CakeApplication.java
@@ -0,0 +1,13 @@
+package com.westee.cake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeApplication.class, args);
+	}
+
+}
