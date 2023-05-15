dfae4e7e66a65bd587b34f3a2d0674730a85ab3a
dflk <904885508@qq.com>
2023-05-15 23:41:09 +0800

    添加PageHelper;完成商品分类的增删改查。

--- src/main/java/com/westee/cake/controller/GoodsTypeController.java
+++ src/main/java/com/westee/cake/controller/GoodsTypeController.java
@@ -1,15 +1,14 @@
 package com.westee.cake.controller;
 
+import com.westee.cake.entity.PageResponse;
 import com.westee.cake.entity.Response;
 import com.westee.cake.generate.GoodsTypes;
 import com.westee.cake.service.GoodsTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/")
+@RestController
+@RequestMapping("/api/v1/")
 public class GoodsTypeController {
     private final GoodsTypeService goodsTypeService;
 
@@ -22,6 +21,24 @@ public class GoodsTypeController {
     public Response<GoodsTypes> createGoodsType(@RequestParam("typename") String typeName,
                                                 @RequestParam("shop-id") Long shopId) {
         GoodsTypes goodsType = goodsTypeService.createGoodsType(typeName, shopId);
-        return Response.of("", goodsType);
+        return Response.of("创建成功", goodsType);
+    }
+
+    @GetMapping("/goods-type")
+    public PageResponse<GoodsTypes> getShop(@RequestParam(name = "pageNum", defaultValue = "1", required = false) Integer pageNum,
+                                       @RequestParam(name = "pageSize", defaultValue = "10", required = false) Integer pageSize,
+                                       @RequestParam(name = "shop-id") Long shopId) {
+
+        return goodsTypeService.getGoodsTypesByShopId(pageNum, pageSize, shopId);
+    }
+
+    @PatchMapping("/goods-type")
+    public Response<GoodsTypes> updateGoodsTypes(@RequestBody GoodsTypes goodsTypes) {
+        return Response.of("更新成功",goodsTypeService.updateGoodsTypes(goodsTypes));
+    }
+
+    @DeleteMapping("/goods-type")
+    public Response<GoodsTypes> deleteGoodsTypes(@RequestBody GoodsTypes goodsTypes) {
+        return Response.of("删除成功", goodsTypeService.deleteGoodsTypes(goodsTypes)) ;
     }
 }
--- src/main/java/com/westee/cake/service/GoodsTypeService.java
+++ src/main/java/com/westee/cake/service/GoodsTypeService.java
@@ -1,19 +1,27 @@
 package com.westee.cake.service;
 
-import com.westee.cake.generate.GoodsTypes;
-import com.westee.cake.generate.GoodsTypesMapper;
+import com.github.pagehelper.PageHelper;
+import com.westee.cake.entity.GoodsStatus;
+import com.westee.cake.entity.PageResponse;
+import com.westee.cake.entity.Response;
+import com.westee.cake.exceptions.HttpException;
+import com.westee.cake.generate.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class GoodsTypeService {
     GoodsTypesMapper goodsTypesMapper;
+    ShopMapper shopMapper;
 
     @Autowired
-    public GoodsTypeService(GoodsTypesMapper goodsTypesMapper) {
+    public GoodsTypeService(GoodsTypesMapper goodsTypesMapper,ShopMapper shopMapper) {
         this.goodsTypesMapper = goodsTypesMapper;
+        this.shopMapper = shopMapper;
     }
 
     public GoodsTypes createGoodsType(String typeName, long shopId) {
@@ -24,5 +32,33 @@ public class GoodsTypeService {
         goodsTypes.setUpdatedAt(new Date());
         long insert = goodsTypesMapper.insert(goodsTypes);
         return goodsTypesMapper.selectByPrimaryKey(insert);
+    }
+
+    public PageResponse<GoodsTypes> getGoodsTypesByShopId(Integer pageNum, Integer pageSize, Long shopId) {
+        User sessionUser = UserService.getSessionUser();
+        Long userId = sessionUser.getId();
+
+        Shop shop = shopMapper.selectByPrimaryKey(shopId);
+        if(shop != null && Objects.equals(shop.getOwnerUserId(), userId)) {
+            GoodsTypesExample goodsTypesExample = new GoodsTypesExample();
+            goodsTypesExample.createCriteria().andOwnerShopIdEqualTo(shopId).andDeletedIsNull();
+            long count = goodsTypesMapper.countByExample(goodsTypesExample);
+            PageHelper.startPage(pageNum, pageSize);
+            List<GoodsTypes> goodsTypes = goodsTypesMapper.selectByExample(goodsTypesExample);
+            return PageResponse.pageData(pageNum, pageSize, count, goodsTypes);
+        } else {
+            throw HttpException.forbidden("没有权限");
+        }
+    }
+
+    public GoodsTypes updateGoodsTypes(GoodsTypes goodsTypes) {
+        int insertId = goodsTypesMapper.updateByPrimaryKey(goodsTypes);
+        return goodsTypesMapper.selectByPrimaryKey((long) insertId);
+    }
+
+    public GoodsTypes deleteGoodsTypes(GoodsTypes goodsTypes) {
+        goodsTypes.setDeleted(1);
+        int insertId = goodsTypesMapper.updateByPrimaryKey(goodsTypes);
+        return goodsTypesMapper.selectByPrimaryKey((long) insertId);
     }
 }
