cb9c8c4bc3ed6a80ab50d0fc63d1d3a52b36565b
dflk <904885508@qq.com>
2023-05-24 21:49:03 +0800

    修改商品分类的bug及鉴权

--- src/main/java/com/westee/cake/controller/GoodsTypeController.java
+++ src/main/java/com/westee/cake/controller/GoodsTypeController.java
@@ -1,11 +1,20 @@
 package com.westee.cake.controller;
 
-import com.westee.cake.entity.PageResponse;
 import com.westee.cake.entity.Response;
+import com.westee.cake.entity.ResponseMessage;
 import com.westee.cake.generate.GoodsTypes;
 import com.westee.cake.service.GoodsTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -21,24 +30,25 @@ public class GoodsTypeController {
     public Response<GoodsTypes> createGoodsType(@RequestParam("typename") String typeName,
                                                 @RequestParam("shop-id") Long shopId) {
         GoodsTypes goodsType = goodsTypeService.createGoodsType(typeName, shopId);
-        return Response.of("创建成功", goodsType);
+        return Response.of(ResponseMessage.OK.toString(), goodsType);
     }
 
     @GetMapping("/goods-type")
-    public PageResponse<GoodsTypes> getShop(@RequestParam(name = "pageNum", defaultValue = "1", required = false) Integer pageNum,
-                                       @RequestParam(name = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-                                       @RequestParam(name = "shop-id") Long shopId) {
-
-        return goodsTypeService.getGoodsTypesByShopId(pageNum, pageSize, shopId);
+    public Response<List<GoodsTypes>> getShop(@RequestParam(name = "shop-id") Long shopId) {
+         return Response.of(ResponseMessage.OK.toString(), goodsTypeService.getGoodsTypesByShopId(shopId));
     }
 
     @PatchMapping("/goods-type")
     public Response<GoodsTypes> updateGoodsTypes(@RequestBody GoodsTypes goodsTypes) {
-        return Response.of("更新成功",goodsTypeService.updateGoodsTypes(goodsTypes));
+        return Response.of(ResponseMessage.OK.toString(), goodsTypeService.updateGoodsTypes(goodsTypes));
     }
 
     @DeleteMapping("/goods-type")
-    public Response<GoodsTypes> deleteGoodsTypes(@RequestBody GoodsTypes goodsTypes) {
-        return Response.of("删除成功", goodsTypeService.deleteGoodsTypes(goodsTypes)) ;
+    public Response<GoodsTypes> deleteGoodsTypes(@RequestParam("id") Long id,
+                                                 @RequestParam("ownerShopId") Long shopId) {
+        GoodsTypes goodsTypes = new GoodsTypes();
+        goodsTypes.setOwnerShopId(shopId);
+        goodsTypes.setId(id);
+        return Response.of(ResponseMessage.OK.toString(), goodsTypeService.deleteGoodsTypes(goodsTypes));
     }
 }
--- src/main/java/com/westee/cake/service/GoodsTypeService.java
+++ src/main/java/com/westee/cake/service/GoodsTypeService.java
@@ -1,11 +1,12 @@
 package com.westee.cake.service;
 
-import com.github.pagehelper.PageHelper;
-import com.westee.cake.entity.GoodsStatus;
-import com.westee.cake.entity.PageResponse;
-import com.westee.cake.entity.Response;
 import com.westee.cake.exceptions.HttpException;
-import com.westee.cake.generate.*;
+import com.westee.cake.generate.GoodsTypes;
+import com.westee.cake.generate.GoodsTypesExample;
+import com.westee.cake.generate.GoodsTypesMapper;
+import com.westee.cake.generate.Shop;
+import com.westee.cake.generate.ShopMapper;
+import com.westee.cake.generate.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class GoodsTypeService {
     ShopMapper shopMapper;
 
     @Autowired
-    public GoodsTypeService(GoodsTypesMapper goodsTypesMapper,ShopMapper shopMapper) {
+    public GoodsTypeService(GoodsTypesMapper goodsTypesMapper, ShopMapper shopMapper) {
         this.goodsTypesMapper = goodsTypesMapper;
         this.shopMapper = shopMapper;
     }
@@ -30,35 +31,42 @@ public class GoodsTypeService {
         goodsTypes.setOwnerShopId(shopId);
         goodsTypes.setCreatedAt(new Date());
         goodsTypes.setUpdatedAt(new Date());
-        long insert = goodsTypesMapper.insert(goodsTypes);
-        return goodsTypesMapper.selectByPrimaryKey(insert);
+        checkBelongToUser(goodsTypes);
+        goodsTypesMapper.insert(goodsTypes);
+        return goodsTypes;
     }
 
-    public PageResponse<GoodsTypes> getGoodsTypesByShopId(Integer pageNum, Integer pageSize, Long shopId) {
-        User sessionUser = UserService.getSessionUser();
-        Long userId = sessionUser.getId();
-
-        Shop shop = shopMapper.selectByPrimaryKey(shopId);
-        if(shop != null && Objects.equals(shop.getOwnerUserId(), userId)) {
-            GoodsTypesExample goodsTypesExample = new GoodsTypesExample();
-            goodsTypesExample.createCriteria().andOwnerShopIdEqualTo(shopId).andDeletedIsNull();
-            long count = goodsTypesMapper.countByExample(goodsTypesExample);
-            PageHelper.startPage(pageNum, pageSize);
-            List<GoodsTypes> goodsTypes = goodsTypesMapper.selectByExample(goodsTypesExample);
-            return PageResponse.pageData(pageNum, pageSize, count, goodsTypes);
-        } else {
-            throw HttpException.forbidden("没有权限");
-        }
+    public List<GoodsTypes> getGoodsTypesByShopId(Long shopId) {
+        GoodsTypesExample goodsTypesExample = new GoodsTypesExample();
+        goodsTypesExample.createCriteria().andOwnerShopIdEqualTo(shopId).andDeletedIsNull();
+        List<GoodsTypes> goodsTypes = goodsTypesMapper.selectByExample(goodsTypesExample);
+        return goodsTypes;
     }
 
     public GoodsTypes updateGoodsTypes(GoodsTypes goodsTypes) {
-        int insertId = goodsTypesMapper.updateByPrimaryKey(goodsTypes);
-        return goodsTypesMapper.selectByPrimaryKey((long) insertId);
+        checkBelongToUser(goodsTypes);
+        goodsTypesMapper.updateByPrimaryKey(goodsTypes);
+        return goodsTypes;
     }
 
     public GoodsTypes deleteGoodsTypes(GoodsTypes goodsTypes) {
+        checkBelongToUser(goodsTypes);
         goodsTypes.setDeleted(1);
-        int insertId = goodsTypesMapper.updateByPrimaryKey(goodsTypes);
-        return goodsTypesMapper.selectByPrimaryKey((long) insertId);
+        goodsTypesMapper.updateByPrimaryKeySelective(goodsTypes);
+        return goodsTypes;
+    }
+
+    public void checkBelongToUser(GoodsTypes goodsTypes) {
+        // 根据goods的shop查询当前用户是不是店铺的拥有者
+        User sessionUser = UserService.getSessionUser();
+        Long userId = sessionUser.getId();
+
+        Shop shopResult = shopMapper.selectByPrimaryKey(goodsTypes.getOwnerShopId());
+        if (shopResult == null) {
+            throw HttpException.forbidden("参数不合法");
+        }
+        if (!Objects.equals(shopResult.getOwnerUserId(), userId)) {
+            throw HttpException.forbidden("拒绝访问");
+        }
     }
 }
