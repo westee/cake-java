3166be4628b1eff72b4944ece18dc69dd97d94ba
dflk <904885508@qq.com>
2023-06-05 20:12:09 +0800

    增加根据token获取用户的接口

--- src/main/java/com/westee/cake/controller/UserController.java
+++ src/main/java/com/westee/cake/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.westee.cake.controller;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.westee.cake.entity.Response;
 import com.westee.cake.entity.ResponseMessage;
 import com.westee.cake.exceptions.HttpException;
 import com.westee.cake.generate.User;
+import com.westee.cake.realm.JWTUtil;
 import com.westee.cake.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,17 @@ public class UserController {
             throw HttpException.forbidden("没有权限");
         }
         return Response.of(ResponseMessage.OK.toString(), userService.updateUser(user)) ;
+    }
+
+    @GetMapping("user")
+    public Response<User> getUser(@RequestHeader("Token") String token) {
+        try {
+            JWTUtil.verify(token);
+        } catch (JWTVerificationException e) {
+            throw  HttpException.notAuthorized("认证失败");
+        }
+        String openid = JWTUtil.getUsername(token);
+        User byOpenid = userService.getByOpenid(openid);
+        return Response.of(ResponseMessage.OK.toString(), byOpenid);
     }
 }
