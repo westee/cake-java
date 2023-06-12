b18f9f55c927bcb3a9e2b1b1fa44e574fb27fe66
dflk <904885508@qq.com>
2023-06-12 16:32:59 +0800

    添加地址表，生成代码

---
+++ src/main/resources/db/migration/V16__CreateAddress.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ADDRESS
+(
+    ID               BIGINT PRIMARY KEY AUTO_INCREMENT,
+    USER_ID          BIGINT,
+    CONTACT          VARCHAR(20),                  -- 联系人
+    ADDRESS          VARCHAR(100),                 -- 小程序中返回的address
+    NAME             VARCHAR(100),                 -- 小程序中返回的name
+    LATITUDE         DECIMAL(10, 6),
+    LONGITUDE        DECIMAL(10, 6),
+    PHONE_NUMBER     VARCHAR(11),
+    SPECIFIC_ADDRESS VARCHAR(50),                  -- 门牌号之类的具体地址
+    GENDER           TINYINT,                      -- 0男 ； 1 女
+    DELETED          INT                DEFAULT 0, -- 0 未删除; 1 已删除
+    CREATED_AT       TIMESTAMP NOT NULL DEFAULT NOW(),
+    UPDATED_AT       TIMESTAMP NOT NULL DEFAULT NOW()
+)
