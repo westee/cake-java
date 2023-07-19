69c7a70f0aa3b4789801528d8e069afa3ff068c0
dflk <904885508@qq.com>
2023-07-19 23:41:11 +0800

    添加加载自定义yml文件配置的方法及其配置

---
+++ src/main/java/com/westee/cake/entity/WeChatConfig.java
@@ -0,0 +1,38 @@
+package com.westee.cake.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:wechatConfig.yml")
+@ConfigurationProperties(prefix = "wechat.miniapp")
+public class WeChatConfig {
+    private String appid;
+    private String secret;
+    private String mchid;
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getMchid() {
+        return mchid;
+    }
+
+    public void setMchid(String mchid) {
+        this.mchid = mchid;
+    }
+}
