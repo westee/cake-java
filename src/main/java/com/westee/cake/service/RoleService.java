c9034e74b13998054323861248b65635426f9c7d
dflk <904885508@qq.com>
2023-08-21 23:52:44 +0800

    订单Role constructor；添加获取用户角色方法

--- src/main/java/com/westee/cake/service/RoleService.java
+++ src/main/java/com/westee/cake/service/RoleService.java
@@ -5,6 +5,7 @@ import com.westee.cake.generate.UserRoleMapper;
 import com.westee.cake.generate.RoleMapper;
 import com.westee.cake.generate.UserRoleExample;
 import com.westee.cake.generate.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,17 +14,25 @@ import java.util.List;
 @Service
 public class RoleService {
 
-    private UserRoleMapper userRoleMapper;
-    private RoleMapper roleMapper;
+    private final UserRoleMapper userRoleMapper;
+    private final RoleMapper roleMapper;
+
+    @Autowired
+    public RoleService(UserRoleMapper userRoleMapper, RoleMapper roleMapper) {
+        this.userRoleMapper = userRoleMapper;
+        this.roleMapper = roleMapper;
+    }
 
     public List<Role> findByUserid(Long userId){
         UserRoleExample userRoleExample = new UserRoleExample();
         userRoleExample.createCriteria().andUserIdEqualTo(userId);
         List<UserRole> userRoles = userRoleMapper.selectByExample(userRoleExample);
         ArrayList<Role> objects = new ArrayList<>();
-        userRoles.forEach(userRole -> {
-            objects.add(roleMapper.selectByPrimaryKey(userRole.getRoleId()));
-        });
+        userRoles.forEach(userRole -> objects.add(roleMapper.selectByPrimaryKey(userRole.getRoleId())));
         return objects;
+    }
+
+    public Role getUserRoleById(Long roleId) {
+        return roleMapper.selectByPrimaryKey(roleId);
     }
 }
