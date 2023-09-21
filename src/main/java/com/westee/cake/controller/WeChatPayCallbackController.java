0e6b39b6c6c0355e57382d49e12e740846cebfd8
dflk <904885508@qq.com>
2023-09-21 10:45:14 +0800

    重构微信支付回调

--- src/main/java/com/westee/cake/controller/WeChatPayCallbackController.java
+++ src/main/java/com/westee/cake/controller/WeChatPayCallbackController.java
@@ -1,16 +1,11 @@
 package com.westee.cake.controller;
 
-import com.wechat.pay.java.service.payments.model.Transaction;
 import com.wechat.pay.java.service.refund.model.RefundNotification;
 import com.wechat.pay.java.service.refund.model.Status;
-import com.westee.cake.data.ChargeStatus;
 import com.westee.cake.entity.OrderStatus;
-import com.westee.cake.entity.OrderType;
-import com.westee.cake.generate.Charge;
 import com.westee.cake.generate.OrderTable;
-import com.westee.cake.service.ChargeService;
 import com.westee.cake.service.OrderService;
-import com.westee.cake.service.UserService;
+import com.westee.cake.service.WeChatPayCallBackService;
 import com.westee.cake.util.WxPayUtil;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
@@ -25,7 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -34,19 +28,17 @@ import java.util.Objects;
 @RequestMapping("/notify")
 public class WeChatPayCallbackController {
     private final OrderService orderService;
-    private final ChargeService chargeService;
-    private final UserService userService;
+    private final WeChatPayCallBackService weChatPayCallBackService;
     private final WxPayUtil wxPayUtil;
 
 
     private static final Logger log = LoggerFactory.getLogger(WeChatPayCallbackController.class);
 
     @Autowired
-    public WeChatPayCallbackController(OrderService orderService,
-                                       ChargeService chargeService, UserService userService, WxPayUtil wxPayUtil) {
+    public WeChatPayCallbackController(OrderService orderService,WeChatPayCallBackService weChatPayCallBackService,
+                                       WxPayUtil wxPayUtil) {
         this.orderService = orderService;
-        this.chargeService = chargeService;
-        this.userService = userService;
+        this.weChatPayCallBackService = weChatPayCallBackService;
         this.wxPayUtil = wxPayUtil;
     }
 
@@ -60,78 +52,7 @@ public class WeChatPayCallbackController {
         log.warn("回调参数body {}", body);
         log.warn("回调参数request {}", request);
         try {
-            Transaction appCallBackVo = wxPayUtil.getNotificationParser(request, body, Transaction.class);
-            log.warn("wx回调参数：{}", appCallBackVo);
-            String outTradeNo = appCallBackVo.getOutTradeNo();
-            String orderType = appCallBackVo.getAttach();
-
-            if (Objects.equals(orderType, OrderType.GOODS.getName())) {
-                OrderTable orderByOrderNo = orderService.getOrderByOutTradeNo(outTradeNo);
-                //  查询我方订单
-                if (Objects.isNull(orderByOrderNo)) {
-                    response.setStatus(500);
-                    log.warn("商品订单 订单不存在 outTradeNo：{}", outTradeNo);
-                    return generateResponseToWxPayCallback("FAIL", "订单不存在");
-                }
-                //        重复回调
-                if (orderByOrderNo.getStatus().equals(OrderStatus.PAID.getName())) {
-                    log.warn("商品订单 重复确认 outTradeNo：{}", outTradeNo);
-                    return generateResponseToWxPayCallback("SUCCESS", "成功-重复");
-                }
-
-                // 金额验证
-                if (new BigDecimal(appCallBackVo.getAmount().getTotal()).compareTo(orderByOrderNo.getTotalPrice().multiply(BigDecimal.valueOf(100))) != 0) {
-                    response.setStatus(500);
-                    log.warn("商品订单 金额不正确 总额（元）：{}", orderByOrderNo.getTotalPrice());
-                    return generateResponseToWxPayCallback("FAIL", "金额不正确");
-                }
-                OrderTable orderSelective = new OrderTable();
-                orderSelective.setId(orderByOrderNo.getId());
-                if (appCallBackVo.getTradeState().name().equals(WxPayConstant.SUCCESS.name())) { // Trade_state是大写
-                    log.error("appCallBackVo.getTradeState().name() {}", appCallBackVo.getTradeState().name().equals(WxPayConstant.SUCCESS.name()));
-                    orderService.deductStockAfterWxPaySuccessByOrderId(orderByOrderNo.getId());
-                    // 更新订单状态
-                    orderSelective.setStatus(OrderStatus.PAID.getName());
-                } else {
-                    orderSelective.setStatus(OrderStatus.FAIL.getName());
-                }
-                if (Objects.nonNull(orderSelective.getStatus())) {
-                    orderService.updateOrderByPrimaryKeySelective(orderSelective);
-                }
-
-            } else {
-                Charge chargeByOutTradeNo = chargeService.getChargeByOutTradeNo(outTradeNo);
-                //  查询我方订单
-                if (Objects.isNull(chargeByOutTradeNo)) {
-                    response.setStatus(500);
-                    log.warn("充值订单 订单不存在 outTradeNo: {}", outTradeNo);
-                    return generateResponseToWxPayCallback("FAIL", "订单不存在");
-                }
-                // 重复回调
-                if (chargeByOutTradeNo.getStatus().equals(ChargeStatus.OK.getName())) {
-                    log.warn("充值订单 重复确认 outTradeNo：{}", outTradeNo);
-                    return generateResponseToWxPayCallback("SUCCESS", "成功-重复");
-                }
-
-                // 金额验证
-                if (new BigDecimal(appCallBackVo.getAmount().getTotal()).compareTo(chargeByOutTradeNo.getAmount().multiply(BigDecimal.valueOf(100))) != 0) {
-                    response.setStatus(500);
-                    log.warn("充值订单 金额不正确 总额（元）：{}", chargeByOutTradeNo.getAmount());
-                    return generateResponseToWxPayCallback("FAIL", "金额不正确");
-                }
-                if (appCallBackVo.getTradeState().name().equals(WxPayConstant.SUCCESS.name())) { // Trade_state是大写
-                    // 更新订单状态
-                    chargeByOutTradeNo.setStatus(ChargeStatus.OK.getName());
-                    log.warn("充值成功:{}", chargeByOutTradeNo);
-                } else {
-                    chargeByOutTradeNo.setStatus(ChargeStatus.OK.getName());
-                    log.warn("充值失败:{}", chargeByOutTradeNo);
-                }
-                chargeService.updateCharge(chargeByOutTradeNo);
-                userService.updateUserBalance(chargeByOutTradeNo);
-            }
-
-            return generateResponseToWxPayCallback("SUCCESS", "成功");
+            return weChatPayCallBackService.dealwithWeixinPay(request, response, body);
         } catch (Exception e) {
             log.error("回调解析错误： {}", e.getMessage());
             response.setStatus(500);
@@ -218,6 +139,4 @@ public class WeChatPayCallbackController {
             }
         }
     }
-
-
 }
