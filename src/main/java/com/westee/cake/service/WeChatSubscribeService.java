33ef5d21ac997703f09184c0399d1d04b066cfb7
dflk <904885508@qq.com>
2023-10-05 14:34:01 +0800

    添加发送小程序微信订阅消息。

--- src/main/java/com/westee/cake/service/WeChatSubscribeService.java
+++ src/main/java/com/westee/cake/service/WeChatSubscribeService.java
@@ -2,30 +2,35 @@ package com.westee.cake.service;
 
 import com.westee.cake.global.GlobalVariable;
 import com.westee.cake.util.RequestUtil;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
+@Service
 public class WeChatSubscribeService {
 
-    public void sendSubscribe(String userOpenId, String templateId) throws IOException {
+    public Object sendSubscribe(String userOpenId, String templateId, HashMap<String, Object> data) throws IOException {
         String url = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=" + GlobalVariable.INSTANCE.getAccessToken();
         HashMap<String, Object> params = new HashMap<>();
         params.put("touser", userOpenId); // openid
         params.put("template_id", templateId); // TEMPLATE_ID
         params.put("page", "page/order/list"); // TEMPLATE_ID
-        HashMap<String, Object> data = new HashMap<>();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
-        Date date = new Date();
-        String formattedDate = formatter.format(date);
-        data.put("date4", formattedDate);
-        data.put("thing5", "商品名");
-        data.put("name8", "x");
-        data.put("thing3", "y");
-        data.put("amount12", "￥" + 10.01);
-        params.put("data", data);
-        RequestUtil.doPost(url, params, null);
+        if(Objects.nonNull(data)) {
+            HashMap<String, Object> map = new HashMap<>();
+            for (Map.Entry<String, Object> entry : data.entrySet()) {
+                map.put(entry.getKey(), getParam(entry.getValue()));
+            }
+            params.put("data", map);
+        }
+        return RequestUtil.doNormalPost(url, params, null);
+    }
+
+    public HashMap<String, Object> getParam(Object p) {
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("value", p);
+        return stringObjectHashMap;
     }
 }
